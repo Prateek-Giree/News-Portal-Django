94d20fc0457bd55bbcc25cb6278b3685449ed572
Pratik Giri <prateekgiree7@gmail.com>
2024-07-26 20:26:10 +0545

    dynamic content

--- a_news/urls.py
+++ a_news/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home, news, login
+from .views import home, news, loginView, register, newsByCategory
 
 urlpatterns = [
     path('', home, name='home'),
     path('news/<int:pk>', news, name='news'),
-    path('login/', login, name='login'),
+    path('login/', loginView, name='login'),
+    path('register/', register, name='register'),
+    path('newsByCategory/<str:category>',
+         newsByCategory, name='newsByCategory'),
 ]
--- a_news/views.py
+++ a_news/views.py
@@ -1,20 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import News
 
 # Create your views here.
 
 
 def home(request):
+    page = 'home'
     categories = ['capital', 'nation', 'politics', 'global',
                   'stock', 'sports', 'science_tech', 'weather']
     news_by_category = {category: News.objects.filter(
         category=category) for category in categories}
 
-    return render(request, 'a_news/home.html', {'news_by_category': news_by_category})
+    recent_news = News.objects.order_by('-date')[:5]
+    context = {
+        'news_by_category': news_by_category,
+        'recent_news': recent_news,
+        'page': page,
+    }
+    return render(request, 'a_news/home.html', context)
 
 
 def news(request, pk):
-    # news_category = get_object_or_404(News, pk=pk)
     all_news = News.objects.all()
     news = News.objects.get(pk=pk)
     context = {
@@ -25,5 +35,41 @@ def news(request, pk):
     return render(request, 'a_news/news.html', context)
 
 
-def login(reqest):
-    return render(reqest, 'form.html')
+def newsByCategory(request, category):
+    news = News.objects.filter(category=category)
+    context = {
+        'news': news,
+        'category':  category.capitalize(),
+    }
+    return render(request, 'a_news/category.html', context)
+
+
+def loginView(request):
+    # if user is already logged in then redirect to home. Prevent relogin
+    if request.user.is_authenticated:
+        return redirect("home")
+    
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)  # create session
+                return redirect("home")
+            else:
+                messages.error(request, "Username or password does not match")
+        except User.DoesNotExist:
+            messages.error(request, "User does not exist")
+
+    page = 'login'
+    context = {
+        'page': page,
+    }
+    return render(request, 'form.html', context)
+
+
+def register(request):
+    return render(request, 'form.html')
